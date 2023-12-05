1ad7968247142408ab834fc9702e48beff0ff4e3
amitepic <amitkumar112417@gmail.com>
2023-12-05 13:31:34 +0530

    uploading files on github

---
+++ script.js
@@ -0,0 +1,7 @@
+
+        const timeBar = document.querySelector('.timeBar')
+        setInterval(function () {
+            let date = new Date();
+            timeBar.innerText = date.toLocaleString();
+             console.log(timeBar.innerHTML);
+        },1000)
